97c4dd64ec8467c6f400c27a801bf84d0446a0a2
Somsekhar Dash <somsekhar.dash@in.abb.com>
2020-08-19 20:45:50 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./../index.scss";
+
+function App() {
+  return <h1>dummy</h1>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.min.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Production",
+    }),
+  ],
+};
